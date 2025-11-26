161c9b646ce9d5aaf6beade02a49330c82de449c
silvabell741-del <silvabell741@gmail.com>
2025-11-26 10:57:25 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -107,9 +107,9 @@ export default defineConfig({
         // display_override: Garante fallback elegante se standalone não for suportado
         display_override: ['window-controls-overlay', 'standalone', 'minimal-ui'],
         orientation: 'portrait-primary',
-        scope: 'https://468e367d.hist-ria-acess-vel-app.pages.dev/',
-        start_url: 'https://468e367d.hist-ria-acess-vel-app.pages.dev/',
-        id: 'https://468e367d.hist-ria-acess-vel-app.pages.dev/',
+        scope: 'https://f8c5fd57.hist-ria-acess-vel-app.pages.dev/',
+        start_url: 'https://f8c5fd57.hist-ria-acess-vel-app.pages.dev/',
+        id: 'https://f8c5fd57.hist-ria-acess-vel-app.pages.dev/',
         categories: ['education', 'productivity', 'reference'],
         lang: 'pt-BR',
         dir: 'ltr',
